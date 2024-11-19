0b6f64dd14d5f9b3b8bff965dbb68181053a902d
RcpRohit <168912119+RcpRohit@users.noreply.github.com>
2024-11-19 10:02:40 +0530

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,8 +1,10 @@
+
 import mongoose from "mongoose";
 
 const dbConnect=async()=>{
     try {
        await mongoose.connect(process.env.MONGODB_URL)
+        console.log(process.env.MONGODB_URL);
        console.log("MongoDB connected successfully")
     } catch (error) {
         console.log("Connection failed",error)
